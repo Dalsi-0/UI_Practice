86eba02593e7d518e699232fcaef2b512e9206ff
dalsi <tkdwns8632@gmail.com>
2025-03-24 12:11:41 +0900

    [Feat] 스크립트 생성

---
+++ Assets/01_Scripts/Manager/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance { get; private set; }
+
+    [SerializeField] UIInventory UIInventory;
+    [SerializeField] UIMainMenu UIMainMenu;
+    [SerializeField] UIStatus UIStatus;
+
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
