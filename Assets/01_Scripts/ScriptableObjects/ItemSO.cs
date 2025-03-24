aa09b1893b866a08ba0f3c9c172167c8aade2d2e
dalsi <tkdwns8632@gmail.com>
2025-03-24 17:50:21 +0900

    [Feat] 아이템 SO 추가

---
+++ Assets/01_Scripts/ScriptableObjects/ItemSO.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "NewItem", menuName = "Scriptable Object/Item/Equipment", order = int.MaxValue)]
+public class ItemSO : ScriptableObject
+{
+    [SerializeField] private Sprite itemIcon;
+    [SerializeField] private int atk;
+    [SerializeField] private int def;
+    [SerializeField] private int hp;
+    [SerializeField] private int critical;
+
+    public Sprite ItemIcon => itemIcon;
+    public int Atk => atk;
+    public int Def => def;
+    public int HP => hp;
+    public int Critical => critical;
+
+}
